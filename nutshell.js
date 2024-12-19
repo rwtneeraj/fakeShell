b1cc1c1f031905237e2f14be37567c5e05e5f6db
rwtneeraj <183721769+rwtneeraj@users.noreply.github.com>
2024-12-19 17:26:57 +0530

    add cd ~ command

--- nutshell.js
+++ nutshell.js
@@ -1,29 +1,46 @@
-let path = "neerajsingh@Neerajs-MacBook-Pro ";
-let currentDir = "users/neerajSingh";
+const root = "neerajsingh@Neerajs-MacBook-Pro ";
+let path = "neerajsingh@Neerajs-MacBook-Pro /users/neerajSingh";
+let currentDir = "/users/neerajSingh";
 
 const echoCommmand = function (args) {
   return args.join("");
 };
 
-const cdCommand = function (args) {
-  currentDir += "/" + args;
-  path = path + " " + currentDir + " %";
+const changeDirectory = function (args) {
+  if (args === "..") {
+    currentDir = currentDir.split("/");
+    currentDir.pop();
+    currentDir = currentDir.join("/");
+    args = "";
+  }
+
+  if (args === "~") {
+    path = root;
+    currentDir = "/users/neerajSingh";
+    return;
+  }
+
+  path = root + currentDir + "/" + args;
+  currentDir = currentDir + "/" + args;
+  return;
+};
+
+const executeCommand = function (runCommand, args) {
+  switch (runCommand) {
+    case "echo": return echoCommmand(args);
+    case "cd": return changeDirectory(args.join(""));
+    case "pwd": return currentDir;
+
+    default: return "nutshell : command not found";
+  }
 };
 
 while (true) {
-  const command = prompt(path + currentDir + " %");
+  const command = prompt(path + " %");
   const [runCommand, ...args] = command.split(' ');
+  const result = executeCommand(runCommand, args);
 
-
-  const executeCommand = function (runCommand, args) {
-    switch (runCommand) {
-      case "echo": return echoCommmand(args);
-      case "cd": return cdCommand(args.join(""));
-      case "pwd": return currentDir;
-
-      default : return "zsh : command not found";
-    }
-  };
-
-  console.log(executeCommand(runCommand, args));
+  if (result !== undefined) {
+    console.log(result);
+  }
 }
