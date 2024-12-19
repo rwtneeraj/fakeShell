96381c34682fbbee6b3d3d319ea846651c2565d6
rwtneeraj <183721769+rwtneeraj@users.noreply.github.com>
2024-12-19 15:42:35 +0530

    add pwd command

--- nutshell.js
+++ nutshell.js
@@ -1,23 +1,27 @@
-
-const path = "neerajsingh@Neerajs-MacBook-Pro";
+let path = "neerajsingh@Neerajs-MacBook-Pro ";
+let currentDir = "users/neerajSingh";
 
 const echoCommmand = function (args) {
-  return args;
+  return args.join("");
 };
 
 const cdCommand = function (args) {
-  return path + " /" + args + " %";
+  currentDir += "/" + args;
+  path = path + " " + currentDir + " %";
 };
 
 while (true) {
-  const command = prompt(path);
+  const command = prompt(path + currentDir + " %");
   const [runCommand, ...args] = command.split(' ');
+
 
   const executeCommand = function (runCommand, args) {
     switch (runCommand) {
       case "echo": return echoCommmand(args);
-      case "cd": return cdCommand(args);
-      case "pwd": return path;
+      case "cd": return cdCommand(args.join(""));
+      case "pwd": return currentDir;
+
+      default : return "zsh : command not found";
     }
   };
 
